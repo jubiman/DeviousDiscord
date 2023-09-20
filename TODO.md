495b96ae08e0341bc8a7402cd53d23a3ca42164f
Melle Bosboom <jubiman@users.noreply.github.com>
2023-09-20 14:10:10 +0200

    Add javadoc

--- TODO.md
+++
@@ -1,3 +0,0 @@
-TODO:
- - [ ] Make channels for the chat
-   - Global and server only (default)
---
+++ src/main/java/com/jubiman/devious/discord/forge/v1_20_1/deviousdiscord/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the main class for the mod, as well as the config class.
+ */
+package com.jubiman.devious.discord.forge.v1_20_1.deviousdiscord;
