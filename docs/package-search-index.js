c77871cb98f13b889cb8318798f82ca00ad74ac6
Melle Bosboom <jubiman@users.noreply.github.com>
2023-09-20 15:30:27 +0200

    Update main branch not to include code

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.jubiman.devious.discord.forge.v1_20_1.deviousdiscord"},{"l":"com.jubiman.devious.discord.forge.v1_20_1.deviousdiscord.network"}];updateSearchResults();
--- src/main/java/com/jubiman/devious/discord/forge/v1_20_1/deviousdiscord/network/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Network classes for Devious Discord integration.
- */
-package com.jubiman.devious.discord.forge.v1_20_1.deviousdiscord.network;
