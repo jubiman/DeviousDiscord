ed795471fe56dac6397ca8386c86584a40628117
Mass1milian0 <maxymino@hotmail.it>
2023-09-24 13:19:14 +0100

    Refactor message format and handle unknown messages
    - Refactored the `server_message_format` and `global_message_format` in `Config.java` to use different placeholders, colors, and formatting styles.
    - Modified the `handle` method in `MessageEvent.java` to replace the new placeholders in the message format

--- src/main/java/com/jubiman/devious/discord/forge/v1_20_1/deviousdiscord/network/events/MessageEvent.java
+++ src/main/java/com/jubiman/devious/discord/forge/v1_20_1/deviousdiscord/network/events/MessageEvent.java
@@ -12,15 +12,19 @@ public class MessageEvent implements Event {
 	@Override
 	public void handle(WebSocket webSocket, JsonObject json) {
 		if (json.has("channel") && json.get("channel").getAsString().equals("global")) {
-			ChannelHandler.sendMessageToGlobalChannel(Config.getGlobalMessageFormat().replaceAll("(?<!\\\\)%s", Config.getIdentifier())
-					.replaceAll("(?<!\\\\)%u", json.get("username").getAsString())
-					.replaceAll("(?<!\\\\)%m", json.get("message").getAsString()));
+			ChannelHandler.sendMessageToGlobalChannel(
+					Config.getGlobalMessageFormat().replaceAll("(?<!\\\\)%s", Config.getIdentifier())
+							.replaceAll("(?<!\\\\)%d", json.get("serverName").getAsString())
+							.replaceAll("(?<!\\\\)%u", json.get("username").getAsString())
+							.replaceAll("(?<!\\\\)%m", json.get("message").getAsString()));
 		} else if (json.has("channel") && json.get("channel").getAsString().equals("server")) {
-			ChannelHandler.sendMessageToServerChannel(Config.getServerMessageFormat().replaceAll("(?<!\\\\)%s", Config.getIdentifier())
-					.replaceAll("(?<!\\\\)%u", json.get("username").getAsString())
-					.replaceAll("(?<!\\\\)%m", json.get("message").getAsString()));
+			ChannelHandler.sendMessageToServerChannel(
+					Config.getServerMessageFormat().replaceAll("(?<!\\\\)%s", Config.getIdentifier())
+							.replaceAll("(?<!\\\\)%u", json.get("username").getAsString())
+							.replaceAll("(?<!\\\\)%m", json.get("message").getAsString()));
 		} else {
-			DeviousDiscord.LOGGER.debug("Received unknown message from Devious Socket: " + json.get("message").getAsString());
+			DeviousDiscord.LOGGER
+					.debug("Received unknown message from Devious Socket: " + json.get("message").getAsString());
 		}
 	}
 }
