47be025420409065bf9e538b9daa5e5ce508f5a0
Melle Bosboom <jubiman@users.noreply.github.com>
2023-09-26 14:00:22 +0200

    Fix lazy copy paste bug

--- src/main/java/com/jubiman/devious/discord/forge/v1_20_1/deviousdiscord/DeviousDiscord.java
+++ src/main/java/com/jubiman/devious/discord/forge/v1_20_1/deviousdiscord/DeviousDiscord.java
@@ -132,7 +132,7 @@ public class DeviousDiscord {
 
 	@SubscribeEvent
 	public void onPlayerLeave(PlayerEvent.PlayerLoggedOutEvent event) {
-		connection.sendPlayerEvent(event.getEntity().getName().getString(), event.getEntity().getUUID(), true);
+		connection.sendPlayerEvent(event.getEntity().getName().getString(), event.getEntity().getUUID(), false);
 
 		ChannelHandler.removeChannel(event.getEntity().getUUID());
 	}
